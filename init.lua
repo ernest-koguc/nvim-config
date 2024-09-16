0c45cfbf52426287436f709e9397f93110d7bfcf
Ernest Koguc <ernestkoguc@gmail.com>
2024-09-16 22:58:16 +0200

    Added roslyn.nvim plugin

--- init.lua
+++ init.lua
@@ -219,6 +219,14 @@ require('lazy').setup({
     branch = "harpoon2",
     dependencies = { "nvim-lua/plenary.nvim" }
   },
+  {
+    "seblj/roslyn.nvim",
+    event = "VeryLazy"
+  },
+  {
+    'tris203/rzls.nvim',
+    event = "VeryLazy"
+  },
 }, {})
 
 
@@ -379,7 +387,9 @@ vim.defer_fn(function()
   require('nvim-treesitter.configs').setup {
     -- Add languages to be installed here that you want installed for treesitter
     ensure_installed = { 'c_sharp', 'lua', 'javascript', 'typescript' },
-
+    modules = {},
+    sync_install = false,
+    ignore_install = {},
     -- Autoinstall languages that are not installed. Defaults to false (but you can change for yourself!)
     auto_install = false,
 
@@ -464,7 +474,8 @@ vim.api.nvim_set_hl(0, 'DiagnosticUnderlineError', { fg = '#C9463D', underline =
 vim.api.nvim_set_hl(0, '@lsp.type.property.cs', { fg = 'white' })
 vim.api.nvim_set_hl(0, '@lsp.type.interface.cs', { fg = '#CBEAA1' })
 vim.api.nvim_set_hl(0, '@lsp.type.namespace.cs', { fg = 'white' })
-
+vim.api.nvim_set_hl(0, '@variable.member.c_sharp', { fg = 'white' })
+vim.api.nvim_set_hl(0, '@keyword.import.c_sharp', { link = '@keyword' })
 -- [[ Configure LSP ]]
 --  This function gets run when an LSP connects to a particular buffer.
 local on_attach = function(_, bufnr)
@@ -511,15 +522,23 @@ local on_attach = function(_, bufnr)
 end
 
 -- document existing key chains
-require('which-key').register {
-  ['<leader>c'] = { name = '[C]ode', _ = 'which_key_ignore' },
-  ['<leader>d'] = { name = '[D]ocument', _ = 'which_key_ignore' },
-  ['<leader>g'] = { name = '[G]it', _ = 'which_key_ignore' },
-  ['<leader>h'] = { name = 'More git', _ = 'which_key_ignore' },
-  ['<leader>r'] = { name = '[R]ename', _ = 'which_key_ignore' },
-  ['<leader>s'] = { name = '[S]earch', _ = 'which_key_ignore' },
-  ['<leader>w'] = { name = '[W]orkspace', _ = 'which_key_ignore' },
+require('which-key').add {
+    { "<leader>c", group = "[C]ode" },
+    { "<leader>c_", hidden = true },
+    { "<leader>d", group = "[D]ocument" },
+    { "<leader>d_", hidden = true },
+    { "<leader>g", group = "[G]it" },
+    { "<leader>g_", hidden = true },
+    { "<leader>h", group = "More git" },
+    { "<leader>h_", hidden = true },
+    { "<leader>r", group = "[R]ename" },
+    { "<leader>r_", hidden = true },
+    { "<leader>s", group = "[S]earch" },
+    { "<leader>s_", hidden = true },
+    { "<leader>w", group = "[W]orkspace" },
+    { "<leader>w_", hidden = true },
 }
+
 
 -- mason-lspconfig requires that these setup functions are called in this order
 -- before setting up the servers.
@@ -539,12 +558,9 @@ local servers = {
   -- gopls = {},
   -- pyright = {},
   -- rust_analyzer = {},
-  -- tsserver = {},
   -- html = { filetypes = { 'html', 'twig', 'hbs'} },
   --
-  tsserver = {},
   --csharp_ls = {},
-  omnisharp = {},
   lua_ls = {
     Lua = {
       workspace = { checkThirdParty = false },
@@ -576,6 +592,19 @@ mason_lspconfig.setup_handlers {
       filetypes = (servers[server_name] or {}).filetypes,
     }
   end,
+}
+
+-- [[ Configure Roslyn]]
+
+require('roslyn').setup {
+  on_attach = on_attach,
+  capabilities = capabilities
+}
+
+require('rzls').setup {
+  on_attach = on_attach,
+  capabilities = capabilities,
+  path = 'C:/Users/grati/AppData/Local/nvim-data/razor'
 }
 
 -- [[ Configure nvim-cmp ]]
